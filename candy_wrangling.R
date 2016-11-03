83de923536fa18809e54d12cefc203338e5181e9
Julien Brun <brun.julien@gmail.com>
2016-11-02 21:28:12 -0700

    added the names to the list and tried the recoding with plyr::mapvalues

--- candy_wrangling.R
+++ candy_wrangling.R
@@ -1,4 +1,5 @@
-install.packages("tidyverse") # Load all
+# install.packages("tidyverse") # Load all
+# library(plyr)
 library(dplyr)
 library(readr)
 library(stringr)
@@ -8,28 +9,34 @@ library(ggplot2)
 files <- list.files(path = "data/usa",pattern = "candy.csv", full.names = T)
 
 # Read the files
-l <- lapply(files, read_csv)
+candy_data <- lapply(files, read_csv)
+#set the names to the list of dataframes
+candy_data <- setNames(candy_data, 2009:2014)
 
 # combine the dataframes
-d <- bind_rows(l, .id="year") %>%
+candy_usa <- bind_rows(candy_data, .id="year") %>%
+  # Combine the categories column due to header mismatch
   mutate(
     candy = ifelse(is.na(candy), X1, candy)) %>%
-  select(-X1)  %>%
-  mutate(year = as.integer(year) + 2008)
-  # mutate(
-  #   year = as.integer(year),
-  #   year = recode(year, c(`1`=2009,`2`=2010)))
+  # Drop the unamed category column
+  select(-X1) # %>%
+  # Replace the years by the real values
+   # mutate(
+     # year = as.integer(year) + 2008)
+     # year = as.integer(year),
+     # year = recode(year, c(`1`=2009,`2`=2010)))
+     # year = mapvalues(all_candy$year, from = 1:6, to = 2009:2014))
 
 # Filter the Halloween candies
-h <- d %>%
+candy_halloween <- candy_usa %>%
   # filter(candy %in% c(
   #   "SEASONAL NON-CHOCOLATE HALLOWEEN CANDY",
   #   "SEASONAL CHOCOLATE HALLOWEEN CANDY"))
   filter(str_detect(candy,"HALLOWEEN"))
 
 # Plot
-ggplot(h, aes(year, `52 Weeks Ending`, color = candy)) + 
-  geom_point() + 
-  geom_line() + 
+ggplot(candy_halloween, aes(year, `52 Weeks Ending`, group = candy, color = candy)) + 
+  geom_point() +
+  geom_line() +
   theme(legend.position = "bottom") 
 
